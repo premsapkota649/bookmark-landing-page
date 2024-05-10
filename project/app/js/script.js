50555f777c2624c991b0586a79afd96320089e8e
premsapkota649 <161706266+premsapkota649@users.noreply.github.com>
2024-05-10 17:00:32 +0545

    Add files via upload

--- project/app/js/script.js
+++ project/app/js/script.js
@@ -1,20 +1,33 @@
 const btnHamburger = document.querySelector('#btnHamburger');
 const overlay = document.querySelector('#header');
+const imgChange = document.querySelector('.logo_image');
 
-btnHamburger.addEventListener('click', function(){
+btnHamburger.addEventListener('click', menuToggle );
+
+
+
+function menuToggle(){
     if(btnHamburger.classList.contains('open')){
         btnHamburger.classList.remove('open')
     }
     else{
         btnHamburger.classList.add('open')
     }
-} );
 
-overlay.addEventListener('click', function(){
     if(overlay.classList.contains('overlay')){
         overlay.classList.remove('overlay');
+
     }
     else{
         overlay.classList.add('overlay')
     }
-})
+}
+
+function imageChange(){
+    if(btnHamburger.classList.contains('open')){
+        imgChange.src ="images/logo-bookmark.svg";
+    }
+    else{
+        imgChange.src ="images/logo-bookmark-light.svg";
+    }
+}
