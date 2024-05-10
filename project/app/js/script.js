68edd6ab24a40b2b950ab79c70f3ac5bf1494b30
premsapkota649 <161706266+premsapkota649@users.noreply.github.com>
2024-05-10 10:40:04 +0545

    Add files via upload

--- project/app/js/script.js
+++ project/app/js/script.js
@@ -1,1 +1,20 @@
-console.log('hello world')
+const btnHamburger = document.querySelector('#btnHamburger');
+const overlay = document.querySelector('#header');
+
+btnHamburger.addEventListener('click', function(){
+    if(btnHamburger.classList.contains('open')){
+        btnHamburger.classList.remove('open')
+    }
+    else{
+        btnHamburger.classList.add('open')
+    }
+} );
+
+overlay.addEventListener('click', function(){
+    if(overlay.classList.contains('overlay')){
+        overlay.classList.remove('overlay');
+    }
+    else{
+        overlay.classList.add('overlay')
+    }
+})
